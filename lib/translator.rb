f3b12e4db377698118739856839cb2634e2c0a94
Adam Kearney <aklives@hotmail.com>
2019-03-24 06:10:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,14 @@ require 'pry'
 def load_library file_path
   emoticons = YAML.load_file(file_path)
   hash = Hash.new
-  hash[get_meaning] = Hash.new
-  hash[get_emoticon] = Hash.new
+  hash["get_emoticon"] = Hash.new
+  hash["get_meaning"] = Hash.new
 
-  emoticons.each do |k, v|
-    'binding.pry'
+  emoticons.each do |word, emo_array|
+    hash["get_emoticon"][emo_array.first] = emo_array.last
+    hash["get_meaning"][emo_array.last] = word
   end
-
+hash
 end
 
 def get_japanese_emoticon
